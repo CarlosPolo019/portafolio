2ba0ad6225661c8b6b0780392f4e3f86faaf191a
Carlos Manuel Escorcia Polo <carlos@livedesign.org>
2025-07-16 12:44:19 -0500

    remove commented-out Image component in Photo.jsx

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -29,14 +29,14 @@ const Photo = () => {
           }}
           className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten absolute"
         >
-          <Image
+          {/* <Image
             src="/assets/IMG_4583.png"
             priority
             quality={100}
             fill
             alt=""
             className="object-contain"
-          />
+          /> */}
         </motion.div>
 
         <motion.svg
