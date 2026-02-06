0206c1caa5f26881149c897a347187efafcc7c77
Carlos Manuel Escorcia Polo <carlos@livedesign.org>
2026-02-06 03:22:34 -0500

    Fix

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://www.cmescorcia.com',
+  siteUrl: 'https://cmescorcia.com',
   generateRobotsTxt: true,
   sitemapSize: 7000,
   changefreq: 'weekly',
